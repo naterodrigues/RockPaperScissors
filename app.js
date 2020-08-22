1efdbcb74017b89d206d291862f0f6ef8e59f475
Nate Rodrigues <nathan.rodrigues@live.com>
2020-08-22 17:51:11 +1000

    Message

---
+++ app.js
@@ -0,0 +1,4 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score"); /*HTML Variables that store 'DOM' elements */
+const computerScore_span = document.getElementById("computer-score"); /*HTML Variables that store 'DOM' elements */
